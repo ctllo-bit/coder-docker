b11db4f336e08e6381e8eda789c8345eec78c1dd
ctllo-bit <test@example.com>
2026-08-12 01:18:45 +0800

    clear all

--- Makefile
+++ Makefile
@@ -10,15 +10,18 @@ all: build-hash build-proxy pack
 
 build-hash:
 	@echo "==> 正在编译 Installer (code-password-hash)..."
-	@cd app/gocmd/installer && $(GO_ENV) go build -o ../../bin/code-password-hash main.go
+	@cd app/gocmd/installer && $(GO_ENV) go build -o ../../bin/code-password-hash .
 
 build-proxy:
 	@echo "==> 正在编译 Proxy (coder-proxy-linux-amd64)..."
-	@cd app/gocmd/proxy && $(GO_ENV) go build -o ../../bin/coder-proxy-linux-amd64 main.go
+	@cd app/gocmd/proxy && $(GO_ENV) go build -o ../../bin/coder-proxy-linux-amd64 .
 
 pack:
+	@rm -f coder-docker.fpk
 	@echo "==> 正在打包 fpk..."
 	@fnpack build
+	@rm -f $(BIN_DIR)/code-password-hash
+	@rm -f $(BIN_DIR)/coder-proxy-linux-amd64
 	@echo "==> 打包完成！"
 
 clean:
--- app/gocmd/proxy/main.go
+++ app/gocmd/proxy/main.go
@@ -143,6 +143,31 @@ func main() {
 	// 程序运行至此，由于有 defer，两个 Socket 都会被安全删除
 }
 
+func initOnce() {
+	flagFile := "/home/coder/.target/.initialized"
+
+	// 已初始化，直接跳过
+	if _, err := os.Stat(flagFile); err == nil {
+		log.Println("initialization already completed, skip")
+		return
+	}
+
+	log.Println("first initialization started")
+
+	// 确保项目路径的权限正确
+	os.Chmod("/home/coder/project", 0750)
+
+	// 创建标记
+	file, err := os.Create(flagFile)
+	if err != nil {
+		log.Printf("failed to create init flag: %v", err)
+		return
+	}
+	defer file.Close()
+
+	log.Println("first initialization completed")
+}
+
 func startCodeSocket(ctx context.Context, socketPath string) *exec.Cmd {
 	node := "/usr/lib/code-server/lib/node"
 	entry := "/usr/lib/code-server/out/node/entry.js"
@@ -153,7 +178,7 @@ func startCodeSocket(ctx context.Context, socketPath string) *exec.Cmd {
 		"/home/coder/project",
 	}
 
-	sureProjectPermission()
+	initOnce()
 	cmd := exec.CommandContext(ctx, node, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -167,17 +192,6 @@ func startCodeSocket(ctx context.Context, socketPath string) *exec.Cmd {
 	}
 
 	return cmd
-}
-
-// 确保项目路径的权限正确
-func sureProjectPermission() {
-	cmd := exec.Command("chmod", "700", "/home/coder/project")
-
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("chmod failed: %v, output: %s", err, output)
-		return
-	}
 }
 
 // 辅助方法：清理残留 Socket
